bc78545dd500023ac67a4b3f362986cce5bf5e2e
nativacu <nativacu@gmail.com>
2021-12-05 19:16:08 -0400

    adding streetlight

---
+++ src/app/semaphore/semaphore.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-semaphore',
+  templateUrl: './semaphore.component.html',
+  styleUrls: ['./semaphore.component.css']
+})
+export class SemaphoreComponent implements OnInit {
+  @Input() on = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
